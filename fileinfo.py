eef588298121bbbbe65a081cc11175dc85053316
ywangge <2509045463@qq.com>
2020-03-25 16:47:15 +0800

    update1 fileinfo.py

--- fileinfo.py
+++ fileinfo.py
@@ -4,3 +4,20 @@ from tkinter.messagebox import *
 from tkinter import *
 from PIL import Image,ImageTk
 
+#打开文件 
+def openfile():  
+    global filename
+    #利用tf库里的askopenfilename函数得到文件的绝对路径。如果不是很清晰，后面用defaultextension函数添加'txt'来说明文件类型
+    filename = askopenfilename(defaultextension = '.txt')
+    if filename == '':
+        filename=None
+    else:
+        ##利用os.path.basename()函数返回path最后的文件名，即格式
+        window.title(os.path.basename(filename))
+        #清空原来的文档
+        text.delete(1.0,END)
+        f = open(filename,'r')
+        #读入新的文档
+        text.insert(1.0,f.read())
+        f.close()
+        
