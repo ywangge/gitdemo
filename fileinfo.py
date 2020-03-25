65c91d11743c9797a1e979bb1aac512b13076de2
ywangge19 <18377226@buaa.edu.cn>
2020-03-25 18:21:09 +0800

    update4

--- fileinfo.py
+++ fileinfo.py
@@ -48,5 +48,47 @@ def saveas():
     fw.close()
     window.title(os.path.basename(f))
 
+#引入event类来实现键盘上的复制粘贴等操作，然后用generate函数来实现
+
+#复制 
+def copy():
+    text.event_generate('<<Copy>>')
+#粘贴 
+def paste():
+    text.event_generate('<<Paste>>')
+
+#全选 
+def selectAll():
+    #利用tag函数中的add操作，sel表示全选
+    text.tag_add('sel','1.0',END)
+
+#剪切
+def cut():
+    text.event_generate('<<Cut>>')
+
+#撤销
+def redo():
+    text.event_generate('<<Redo>>')
+
+#重做
+def undo():
+    text.event_generate('<<Undo>>')
+
+#查找指定内容的个数 
+def search():
+    def dosearch():
+        wordname = entry1.get()             #获取查找的内容--string型
+        essay = str(text.get(1.0,END))#文本框的内容
+        showinfo(title='查找结果：',message=("查找结果：{}共有{}个").format(wordname,essay.count(wordname)))#直接用count函数找
+        
+    topsearch = Toplevel(window)
+    topsearch.geometry('400x50+150+200')
+    label1 = Label(topsearch,text='请输入需要查找的单词')
+    label1.grid(row=0, column=0,padx=5)
+    entry1 = Entry(topsearch,width=20)
+    entry1.grid(row=0, column=1,padx=5)
+    button1 = Button(topsearch,text='查找',command=dosearch)
+    button1.grid(row=0, column=3)
+
 
         
